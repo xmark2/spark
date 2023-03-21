0b28325becaa635d9ecb0e1a425a65762ab1f6db
xmark2 <xmark2@gmail.com>
2023-03-21 23:43:10 +0100

    exp

--- exp/sp_04_datasink/DataSinkDemo.py
+++ exp/sp_04_datasink/DataSinkDemo.py
@@ -1,70 +1,86 @@
-from pyspark.sql import *
+import sys
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import spark_partition_id, lit
+from lib import Log4j, get_spark_app_config, load_parquet_df
 
-from lib.logger import Log4j
+
+class DataSinkApp:
+
+    def __init__(self, path_conf):
+
+        conf = get_spark_app_config(path_conf)
+
+        self.spark = SparkSession.builder \
+            .config(conf=conf) \
+            .getOrCreate()
+
+        self.logger = Log4j(self.spark)
+
+    def check_arg_data(self):
+        if len(sys.argv) != 2:
+            self.logger.error("Usage: HelloSpark <filename>")
+            sys.exit(-1)
+
+    def get_datasink(self, path_parquet):
+
+        # self.check_arg_data()
+
+        self.logger.info("Starting DataSinkApp to read Parquet")
+
+        # flighttime_csv_df = load_csv_df(self.spark, "data/flight*.csv")
+        flighttime_df = load_parquet_df(self.spark, path_parquet)
+
+        # flighttime_df.show()
+
+        flighttime_df.write \
+            .format("json") \
+            .mode("overwrite") \
+            .option("path", "dataSink/json/") \
+            .partitionBy("OP_CARRIER", "ORIGIN") \
+            .option("maxRecordsPerFile", 10000) \
+            .save()
+
+        self.logger.info("Parquet Schema:" + flighttime_df.schema.simpleString())
+
+        ## while the code running you can open Spark UI
+        ## http://localhost:4040/jobs/
+
+        input("Press Enter")
+
+        self.logger.info("Finished DataSinkApp to read Parquet")
+        self.spark.stop()
+
+    def read_datasink(self, filter_carrier, filter_origin):
+        filter_carrier = 'AA'
+        filter_origin = 'BHM'
+
+        path = f"dataSink/json/OP_CARRIER={filter_carrier}/ORIGIN={filter_origin}/part*"
+
+        df_json = self.spark.read \
+            .format("json") \
+            .option("mode", "FAILFAST") \
+            .load(path) \
+            .withColumn('OP_CARRIER', lit(filter_carrier)) \
+            .withColumn('ORIGIN', lit(filter_origin))
+
+        df_json.createOrReplaceTempView("flight_data")
+
+        self.logger.info("start sql")
+        df_output = self.spark.sql("select * from flight_data")
+        print(df_output.columns)
+        df_output.show()
+        self.logger.info("finished sql")
+
 
 if __name__ == "__main__":
-    spark = SparkSession \
-        .builder \
-        .master("local[3]") \
-        .appName("SparkSchemaDemo") \
-        .getOrCreate()
+    myapp = DataSinkApp(path_conf="spark.conf")
+    myapp.get_datasink(path_parquet="dataSource/flight*.parquet")
 
-    logger = Log4j(spark)
+    myapp = DataSinkApp(path_conf="spark.conf")
+    myapp.read_datasink(filter_origin='BHM', filter_carrier='HP')
 
-    flightTimeParquetDF = spark.read \
-        .format("parquet") \
-        .load("dataSource/flight*.parquet")
-
-    flightTimeParquetDF.createOrReplaceTempView("flight_data")
-
-    print(flightTimeParquetDF.columns)
-
-    logger.info("start sql")
-    spark.sql("select count(*) from flight_data where OP_CARRIER='AA' and ORIGIN='BHM'").show()
-    logger.info("finished sql")
-
-    # logger.info("Num Partitions before: " + str(flightTimeParquetDF.rdd.getNumPartitions()))
-    # flightTimeParquetDF.groupBy(spark_partition_id()).count().show()
-    #
-    # partitionedDF = flightTimeParquetDF.repartition(5)
-    # logger.info("Num Partitions after: " + str(partitionedDF.rdd.getNumPartitions()))
-    # partitionedDF.groupBy(spark_partition_id()).count().show()
-
-    # partitionedDF.write \
-    #     .format("avro") \
-    #     .mode("overwrite") \
-    #     .option("path", "dataSink/avro/") \
-    #     .save()
-
-    # flightTimeParquetDF.write \
-    #     .format("json") \
-    #     .mode("overwrite") \
-    #     .option("path", "dataSink/json/") \
-    #     .partitionBy("OP_CARRIER", "ORIGIN") \
-    #     .option("maxRecordsPerFile", 10000) \
-    #     .save()
-    #
-    filter_carrier = 'AA'
-    filter_origin = 'BHM'
-
-    path = f"dataSink/json/OP_CARRIER={filter_carrier}/ORIGIN={filter_origin}/part*"
-    # newcol = path.split("/")[-1].split("=")
-    # print(newcol)
-    #
-    dfjson = spark.read \
-        .format("json") \
-        .option("mode", "FAILFAST") \
-        .load(path)\
-        .withColumn('OP_CARRIER', lit(filter_carrier))\
-        .withColumn('ORIGIN', lit(filter_origin))
-
-    dfjson.createOrReplaceTempView("flight_data2")
-
-    # print(flightTimeParquetDF.columns)
-
-    logger.info("start sql")
-    df = spark.sql("select * from flight_data2")
-    print(df.columns)
-    df.show()
-    logger.info("finished sql")
+    # # partitionedDF.write \
+    # #     .format("avro") \
+    # #     .mode("overwrite") \
+    # #     .option("path", "dataSink/avro/") \
+    # #     .save()
--- exp/sp_04_datasink/lib/utils.py
+++ exp/sp_04_datasink/lib/utils.py
@@ -3,25 +3,31 @@ import configparser
 from pyspark import SparkConf
 
 
-def load_survey_df(spark, data_file):
-    return spark.read \
-        .option("header", "true") \
-        .option("inferSchema", "true") \
-        .csv(data_file)
+# def load_survey_df(spark, data_file):
+#     return spark.read \
+#         .option("header", "true") \
+#         .option("inferSchema", "true") \
+#         .csv(data_file)
+#
+#
+# def count_by_country(survey_df):
+#     return survey_df.filter("Age < 40") \
+#         .select("Age", "Gender", "Country", "state") \
+#         .groupBy("Country") \
+#         .count()
 
 
-def count_by_country(survey_df):
-    return survey_df.filter("Age < 40") \
-        .select("Age", "Gender", "Country", "state") \
-        .groupBy("Country") \
-        .count()
-
-
-def get_spark_app_config():
+def get_spark_app_config(path_conf):
     spark_conf = SparkConf()
     config = configparser.ConfigParser()
-    config.read("spark.conf")
+    config.read(path_conf)
 
     for (key, val) in config.items("SPARK_APP_CONFIGS"):
         spark_conf.set(key, val)
     return spark_conf
+
+
+def load_parquet_df(spark, data_file):
+    return spark.read \
+        .format("parquet") \
+        .load(data_file)
